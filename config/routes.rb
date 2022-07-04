ce320c6375483e471add852ca453c5648baea79b
pdany1116 <popa.daniel@agilefreaks.com>
2022-07-04 12:52:32 +0300

    Add https for google url

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root :to => redirect('www.google.com')
+  root :to => redirect("https://google.com")
 end
