d5ab14f98a5ebdf9156a3dec9797dae26e7a100e
pdany1116 <popa.daniel@agilefreaks.com>
2022-07-07 11:38:26 +0300

    [6] Remove named subjects

--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -5,31 +5,31 @@ require 'rails_helper'
 RSpec.describe Todo, type: :model do
   describe 'Validations' do
     context 'with valid parameters' do
-      subject(:todo) { build(:todo) }
+      subject { build(:todo) }
 
       it { is_expected.to be_valid }
     end
 
     context 'with invalid title' do
-      subject(:todo) { build(:todo, title: nil) }
+      subject { build(:todo, title: nil) }
 
       it { is_expected.not_to be_valid }
     end
 
     context 'with invalid completed' do
-      subject(:todo) { build(:todo, completed: nil) }
+      subject { build(:todo, completed: nil) }
 
       it { is_expected.not_to be_valid }
     end
 
     context 'with invalid url' do
-      subject(:todo) { build(:todo, url: nil) }
+      subject { build(:todo, url: nil) }
 
       it { is_expected.not_to be_valid }
     end
 
     context 'with invalid order' do
-      subject(:todo) { build(:todo, order: nil) }
+      subject { build(:todo, order: nil) }
 
       it { is_expected.not_to be_valid }
     end
