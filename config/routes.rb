c5721d39924d40ba462ec21f101ba588e8ef1402
pdany1116 <popa.daniel@agilefreaks.com>
2022-07-07 14:20:29 +0300

    [7] Clean routes configuration file

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  root to: redirect('https://google.com')
   resources :todos, only: [:index]
 end
