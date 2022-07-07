ea959ca1e4243de3d697f62e6ea54d2871c6cff5
pdany1116 <popa.daniel@agilefreaks.com>
2022-07-07 17:07:54 +0300

    [5] Allow CORS

--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,16 @@ module TodoApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.middleware.insert_before 0, 'Rack::Cors' do
+      allow do
+        origins '*'
+        resource(
+          '*',
+          headers: :any,
+          methods: %i[get patch delete post options]
+        )
+      end
+    end
   end
 end
