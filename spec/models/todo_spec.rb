b2bab9ff4dda391f562f6220e99324a3354864e6
pdany1116 <popa.daniel@agilefreaks.com>
2022-07-07 11:12:33 +0300

    [6] Use factory to create model objects

--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -4,15 +4,9 @@ require 'rails_helper'
 
 RSpec.describe Todo, type: :model do
   describe 'Validations' do
-    let(:create_todo) { Todo.create(title:, completed:, url:, order:) }
-    let(:title) { Faker::Beer.brand }
-    let(:completed) { false }
-    let(:url) { Faker::Internet.url }
-    let(:order) { 1 }
-
     context 'with valid parameters' do
       it 'is valid' do
-        todo = create_todo
+        todo = build(:todo)
 
         expect(todo).to be_valid
       end
@@ -20,40 +14,32 @@ RSpec.describe Todo, type: :model do
 
     context 'with invalid parameters' do
       context 'with invalid title' do
-        let(:title) { nil }
-
         it 'is invalid' do
-          todo = create_todo
+          todo = build(:todo, title: nil)
 
           expect(todo).not_to be_valid
         end
       end
 
       context 'with invalid completed' do
-        let(:completed) { nil }
-
         it 'is invalid' do
-          todo = create_todo
+          todo = build(:todo, completed: nil)
 
           expect(todo).not_to be_valid
         end
       end
 
       context 'with invalid url' do
-        let(:url) { nil }
-
         it 'is invalid' do
-          todo = create_todo
+          todo = build(:todo, url: nil)
 
           expect(todo).not_to be_valid
         end
       end
 
       context 'with invalid order' do
-        let(:order) { nil }
-
         it 'is invalid' do
-          todo = create_todo
+          todo = build(:todo, order: nil)
 
           expect(todo).not_to be_valid
         end
