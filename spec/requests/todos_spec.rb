3a09cea4e64ca0de48aeb45de3c8ff106c228926
Daniel Popa <popa.daniel@agilefreaks.com>
2022-07-08 16:46:15 +0300

    [7] Separate subject from expectation

--- spec/requests/todos_spec.rb
+++ spec/requests/todos_spec.rb
@@ -9,11 +9,13 @@ RSpec.describe 'Todos', type: :request do
     context 'with no todos added' do
       it 'returns 200' do
         get_todos
+
         expect(response).to have_http_status(:ok)
       end
 
       it 'returns empty array' do
         get_todos
+
         expect(parsed_body).to eq []
       end
     end
@@ -23,11 +25,13 @@ RSpec.describe 'Todos', type: :request do
 
       it 'returns 200' do
         get_todos
+
         expect(response).to have_http_status(:ok)
       end
 
       it 'returns array with 5 todos' do
         get_todos
+
         expect(parsed_body.length).to be 5
       end
     end
