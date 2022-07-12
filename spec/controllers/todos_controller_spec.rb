7545e194cc0b6a41e2d4b9e086c6a336b282e266
pdany1116 <popa.daniel@agilefreaks.com>
2022-07-12 13:28:48 +0300

    [8] Convert request specs to controller specs

--- spec/controllers/todos_controller_spec.rb
+++ spec/controllers/todos_controller_spec.rb
@@ -2,19 +2,19 @@
 
 require 'rails_helper'
 
-RSpec.describe 'Todos', type: :request do
-  describe 'GET /todos' do
-    subject(:get_todos) { get '/todos' }
+RSpec.describe TodosController, type: :controller do
+  describe '#index' do
+    subject { get :index }
 
     context 'with no todos added' do
       it 'returns 200' do
-        get_todos
+        subject
 
         expect(response).to have_http_status(:ok)
       end
 
       it 'returns empty array' do
-        get_todos
+        subject
 
         expect(parsed_body).to eq []
       end
@@ -24,32 +24,37 @@ RSpec.describe 'Todos', type: :request do
       before { create_list(:todo, 5) }
 
       it 'returns 200' do
-        get_todos
+        subject
 
         expect(response).to have_http_status(:ok)
       end
 
       it 'returns array with 5 todos' do
-        get_todos
+        subject
 
         expect(parsed_body.length).to be 5
       end
     end
   end
 
-  describe 'GET /todos/:id' do
-    subject(:get_todo) { get "/todos/#{id}" }
+  describe '#show' do
+    subject { get :show, params: }
+    let(:params) do
+      {
+        id:
+      }
+    end
     let(:id) { 1 }
 
     context 'with not existing todo' do
       it 'returns 404' do
-        get_todo
+        subject
 
         expect(response).to have_http_status(:not_found)
       end
 
       it 'returns empty body' do
-        get_todo
+        subject
 
         expect(response.body).to be_empty
       end
@@ -59,7 +64,7 @@ RSpec.describe 'Todos', type: :request do
       before { create(:todo, id:) }
 
       it 'returns 200' do
-        get_todo
+        subject
 
         expect(response).to have_http_status(:ok)
       end
