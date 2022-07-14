a9bcd47c4a6ea5e688b4345720f4f5174d5a3c22
pdany1116 <popa.daniel@agilefreaks.com>
2022-07-14 11:54:14 +0300

    [9] Use explicit serializer

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -4,13 +4,13 @@ class TodosController < ApplicationController
   def index
     todos = Todo.all
 
-    render json: todos, status: :ok
+    render json: todos, status: :ok, each_serializer: TodoSerializer
   end
 
   def show
     todo = Todo.find(params[:id])
 
-    render json: todo, status: :ok
+    render json: todo, status: :ok, serializer: TodoSerializer
   rescue ActiveRecord::RecordNotFound
     render json: '', status: :not_found
   end
@@ -18,7 +18,7 @@ class TodosController < ApplicationController
   def create
     todo = Todo.create!(create_todo_params)
 
-    render json: todo, status: :created
+    render json: todo, status: :created, serializer: TodoSerializer
   rescue ActionController::ParameterMissing, ActiveRecord::RecordInvalid
     render json: '', status: :unprocessable_entity
   end
