372842bdb35c83fde5d7f1540d947cfae6ee49d3
pdany1116 <popa.daniel@agilefreaks.com>
2022-07-15 15:35:55 +0300

    [24] Fix url missing /todos

--- app/serializers/todo_serializer.rb
+++ app/serializers/todo_serializer.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 class TodoSerializer < ActiveModel::Serializer
+  include Rails.application.routes.url_helpers
+
   attributes :id, :title, :url, :order, :completed
 
   def url
-    "#{ENV.fetch('HOST_URL')}/#{object.id}"
+    "#{ENV.fetch('HOST_URL')}#{todos_path}/#{object.id}"
   end
 end
