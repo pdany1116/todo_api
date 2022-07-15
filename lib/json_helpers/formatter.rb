4f9eaac5494156007ecc7d009ef2d06585e00b08
pdany1116 <popa.daniel@agilefreaks.com>
2022-07-15 10:54:35 +0300

    [9] Create matchers in one line

--- lib/json_helpers/formatter.rb
+++ lib/json_helpers/formatter.rb
@@ -8,8 +8,7 @@ module JsonHelpers
 
     def format_content(args)
       all_matchers = args.keys.join('|')
-      non_string_matchers = args.keys.select { |key| key unless args[key].is_a?(String) }
-      non_string_matchers = non_string_matchers.join('|')
+      non_string_matchers = (args.keys.select { |key| key unless args[key].is_a?(String) }).join('|')
       result = @content.gsub(/"{(#{non_string_matchers})}"/, '%{\1}').gsub(/{(!%#{all_matchers})}/, '%{\1}')
 
       format(result, args)
