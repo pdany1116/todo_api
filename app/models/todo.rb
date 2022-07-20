a69cf8233b5905443dc9ae6018aaf8abcc07f8e9
pdany1116 <popa.daniel@agilefreaks.com>
2022-07-20 16:12:29 +0300

    [26] Add status column for todos

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,9 +1,12 @@
 # frozen_string_literal: true
 
 class Todo < ApplicationRecord
+  enum status: { active: 0, deleted: 1 }
+
   validates :title, presence: true
   validates :completed, inclusion: { in: [true, false] }
   validates :order, numericality: true
+  validates :status, inclusion: { in: Todo.statuses.keys }
 
   attribute :completed, :boolean, default: false
   attribute :order, :integer, default: 0
