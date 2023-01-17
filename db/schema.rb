392a1bd031a2df409b8b483f8f6eadf7ef20c69b
Daniel Popa <popa.daniel@agilefreaks.com>
2023-01-17 18:30:28 +0200

    [27] Fix database connection

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -14,13 +12,14 @@
 
 ActiveRecord::Schema[7.0].define(version: 2022_07_13_140928) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'todos', force: :cascade do |t|
-    t.string 'title'
-    t.boolean 'completed', default: false
-    t.integer 'order', default: 0
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "todos", force: :cascade do |t|
+    t.string "title"
+    t.boolean "completed", default: false
+    t.integer "order", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
+
 end
